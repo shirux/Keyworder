9e579eac425fcb8515c06d2f6c4bd07438698821
Daniel Ramos <shirux123@gmail.com>
2022-07-09 15:11:26 -0500

    interface

--- src/components/Keyworder.js
+++ src/components/Keyworder.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
-import { Grid, TextField } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import React, { useState } from "react";
+import {
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  TextareaAutosize,
+  TextField,
+} from "@mui/material";
+import { ButtonUnstyled } from "@mui/base";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 
 const Keyworder = () => {
-
   const [isLoading, setIsLoading] = useState({});
   const [tags, setTags] = useState({});
   const [limit, setLimit] = useState({});
@@ -22,20 +19,40 @@ const Keyworder = () => {
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <TextField fullWidth id="outlined-basic" label="Tags" variant="filled" />
+      <Grid item xs={7}>
+        <TextField
+          fullWidth
+          id="outlined-basic"
+          label="Palabras"
+          variant="outlined"
+        />
       </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
+      <Grid item xs={2}>
+        <FormControlLabel
+          value="bottom"
+          control={<Checkbox />}
+          label="Limite"
+          labelPlacement="bottom"
+        />
       </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
+      <Grid item xs={3}>
+        <TextField
+          fullWidth
+          id="outlined-basic"
+          type="number"
+          label="Cantidad"
+          variant="filled"
+          defaultValue="50"
+        />
       </Grid>
-      <Grid item xs={8}>
-        <Item>xs=8</Item>
+      <Grid item xs={12}>
+        <ButtonUnstyled>Holi</ButtonUnstyled>
+      </Grid>
+      <Grid item xs={12}>
+        <TextareaAutosize minRows={20} style={{ width: "100%" }} />
       </Grid>
     </Grid>
   );
-}
+};
 
 export default Keyworder;
